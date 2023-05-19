9675cd60f2954b5438e72f8d01918e206e3ff114
Ashraf-Khalifa <ashrafrkhalifa@gmail.com>
2023-05-19 21:39:35 +0300

    done

---
+++ pages/charts/app.js
@@ -0,0 +1,136 @@
+let inputOne = document.getElementById("name");
+let inputTwo = document.getElementById("name2");
+let btn = document.getElementById("btn");
+let winner = document.getElementById("winner");
+let style = document.createElement("style");
+let container = document.getElementById("div");
+
+checkbox.addEventListener("click", function () {
+  changeColor();
+});
+function changeColor() {
+  if (checkbox.checked == true) {
+    document.body.style.background = "black";
+    document.body.style.color = "white";
+
+    sub.style.background = "black";
+    sub.style.color = "white";
+  } else {
+    document.body.style.background = "white";
+    document.body.style.color = "black";
+
+    sub.style.background = "white";
+    sub.style.color = "black";
+  }
+}
+btn.onclick = function () {
+  event.preventDefault();
+
+  if (inputOne.value === "" || inputTwo.value === "") {
+    alert("Enter valid usernames");
+  } else if (inputOne.value === inputTwo.value) {
+    alert("Enter different usernames");
+  } else {
+    const API_REPO_ONE = `https://api.github.com/users/${inputOne.value}/repos`;
+    const API_REPO_TWO = `https://api.github.com/users/${inputTwo.value}/repos`;
+    const API_REPO_ONE_AVATAR = `https://api.github.com/users/${inputOne.value}`;
+    const API_REPO_TWO_AVATAR = `https://api.github.com/users/${inputTwo.value}`;
+
+    fetch(API_REPO_ONE)
+      .then((response) => {
+        return response.json();
+      })
+      .then((dataOne) => {
+        fetch(API_REPO_ONE_AVATAR)
+          .then((response) => {
+            return response.json();
+          })
+          .then((dataOneImage) => {
+            let avatarOne = dataOneImage.avatar_url;
+            fetch(API_REPO_TWO)
+              .then((response) => {
+                return response.json();
+              })
+              .then((dataTwo) => {
+                fetch(API_REPO_TWO_AVATAR)
+                  .then((response) => {
+                    return response.json();
+                  })
+                  .then((dataTwoImage) => {
+                    let avatarTwo = dataTwoImage.avatar_url;
+                    if (dataOne.length >= dataTwo.length) {
+                      container.innerHTML += `
+                        <div class="row">
+                          <div class="container1 col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xs-offset-0">
+                            
+                              <div class="winner">
+                                <h1>winner</h1>
+                                <div class="img1"><img src="${avatarOne}" alt="" style="border-radius: 50%; width: 200px; height: 100%;"></div>
+                                <h3>@${inputOne.value}</h3>
+                              </div>
+                              <div class="loser">
+                                <h1>loser</h1>
+                                <div class="img2"><img src="${avatarTwo}" alt="" style="border-radius: 50%; width: 200px; height: 100%;"></div>
+                                <h3>@${inputTwo.value}</h3>
+                              </div>
+                            
+                          </div>
+                        </div>
+                      `;
+                    } else if (dataTwo.length >= dataOne.length) {
+                      container.innerHTML += `
+                        <div class="row">
+                          <div class="container2 col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xs-offset-0">
+                            
+                              <div class="winner">
+                                <h1>winner</h1>
+                                <div class="img3"><img src="${avatarTwo}" alt="" style="border-radius: 50%; width: 200px; height: 100%;"></div>
+                                <h3>@${inputTwo.value}</h3>
+                              </div>
+                              <div class="loser">
+                                <h1>loser</h1>
+                                <div class="img4"><img src="${avatarOne}" alt="" style="border-radius: 50%; width: 200px; height: 100%;"></div>
+                                <h3>@${inputOne.value}</h3>
+                              </div>
+                           
+                          </div>
+                        </div>
+                      `;
+                    } else {
+                      container.innerHTML = `
+                        <h1>It's a tie!</h1>
+                      `;
+                    }
+
+                    style.innerHTML += `
+                      .container1 {
+                        display: flex;
+                        flex-wrap: nowrap;
+                         grid-template-columns: 1fr 1fr 1fr;
+                         grid-template-rows: 1fr 1fr 1fr;
+                         gap: 150px;
+                      }
+                      container2 {
+                        display: flex;
+                        flex-wrap: nowrap;
+                         grid-template-columns: 1fr 1fr 1fr;
+                         grid-template-rows: 1fr 1fr 1fr;
+                         gap: 150px;
+                      } 
+                      .winner,
+                      .loser {
+                        text-align: center;
+                        gap:100px
+                      }                  
+                    `;
+
+                    document.head.appendChild(style);
+                  });
+              });
+          });
+      })
+      .catch((error) => {
+        document.write("", error);
+      });
+  }
+};
---
+++ pages/charts/chart.js
@@ -0,0 +1,82 @@
+let input = document.getElementById("input");
+let btn = document.getElementById("btn");
+let repo = document.getElementById("repo");
+let sidebar = document.getElementById("sidebar");
+let checkbox = document.getElementById("checkbox");
+let cont = document.getElementById("cont");
+btn.onclick = function () {
+  getRepos();
+};
+checkbox.addEventListener("click", function () {
+  changeColor();
+});
+function changeColor() {
+  if (checkbox.checked == true) {
+    document.body.style.background = "black";
+    document.body.style.color = "white";
+    cont.style.background = "black";
+    sidebar.style.background = "black";
+    sidebar.style.color = "white";
+  } else {
+    document.body.style.background = "white";
+    document.body.style.color = "black";
+    cont.style.background = "white";
+    sidebar.style.background = "white";
+    sidebar.style.color = "black";
+  }
+}
+function getRepos() {
+  if (input.value === "") {
+    alert("Please enter a username");
+  } else {
+    event.preventDefault();
+    let token = "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";
+
+    fetch(`https://api.github.com/users/${input.value}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        sidebar.innerHTML = `
+           <div class="  col-lg-2 col-md-3" id="sidebar"><br><br><br><br><br>
+             <img class="img" src="${response.avatar_url}" width="200" height="200">
+            <h3 class="name" >${response.login}</h3>
+            <button type="button" style="width: 200px; height: 30px; margin: 20px; border-radius: 5px;">Edit profile</button>
+           </div>
+         `;
+
+        changeColor();
+      })
+   
+    fetch(`https://api.github.com/users/${input.value}/repos`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((repositories) => {
+        repo.innerHTML = "";
+        for (let i of repositories) {
+          let newRepo = document.createElement("div");
+          newRepo.className = "col-lg-6 grid-margin stretch-card";
+          newRepo.innerHTML = `
+            <div class="card">
+              <div class="card-body" style="background-color: #ffffff; padding: 1.25rem;">
+                <h5>${i.name}</h5> <br><br>
+                <p><img src="pages/charts/Location_dot_black.svg.png" width="20px" height="20px"> ${i.language}</p>
+              </div>
+            </div>
+          `;
+          changeColor();
+          repo.appendChild(newRepo);
+        }
+        changeColor();
+      })
+        .catch((error) => {
+          document.write("", error);
+        });
+      
+  }
+}
